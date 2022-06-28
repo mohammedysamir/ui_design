111e0a274626e5262bf3d2f965eb9e0219605e83
Mohammed Yasser <mohammedre4a@gmail.com>
2022-06-29 01:57:58 +0200

    feat: added automatic slider for OnBoarding.dart + Register/Get Started/skip button on Click. feat: added brand logo and name.

--- lib/OnBoarding.dart
+++ lib/OnBoarding.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:ui_design/MainButton.dart';
+import 'package:ui_design/Utility_classes/MainButton.dart';
+import '/Utility_classes/OnBoardingSlide.dart';
+import 'HomePage.dart';
+import 'RegisterScreen.dart';
 
 class OnBoarding extends StatefulWidget {
   const OnBoarding({Key? key}) : super(key: key);
@@ -12,6 +16,19 @@ class _OnBoardingState extends State<OnBoarding> {
   final Color blackColor = const Color.fromARGB(255, 46, 46, 46);
   final Color mainWhiteColor = const Color.fromARGB(255, 255, 255, 255);
   final Color tealColor = const Color.fromARGB(255, 81, 175, 171);
+  int currentSlide = 0;
+  List<OnBoardingSlide> slides = [
+    OnBoardingSlide(
+        image: Image.asset("assets/images/delivery.jpg"),
+        mainText: "Get food delivery to your doorstep asap",
+        secondaryText:
+            "We have young and professional delivery team that will bring your food as soon as possible to your doorstep"),
+    OnBoardingSlide(
+        image: Image.asset("assets/images/buy_food.jpg"),
+        mainText: "Buy any food from your favorite restaurant",
+        secondaryText:
+            "We are consistently adding your favorite restaurant throughout the territory and around your area carefully selected"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +38,17 @@ class _OnBoardingState extends State<OnBoarding> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          //skip button
           Row(
-            //skip button
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               ElevatedButton(
                 onPressed: () {
-                  //todo: add skip functionality
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const MyHomePage(title: "Welcome to home page")));
                 },
                 style: ButtonStyle(
                     elevation: MaterialStateProperty.all(0),
@@ -46,15 +67,33 @@ class _OnBoardingState extends State<OnBoarding> {
               ),
             ],
           ),
-          OnBoardingSlide(
-              image: Image.asset("assets/images/delivery.jpg"),
-              mainText: "Get food delivery to your doorstep asap",
-              secondaryText:
-                  "We have young and professional delivery team that will bring your food as soon as possible to your doorstep"),
+          //brand name
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "7",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 233, 183, 119),
+                    decoration: TextDecoration.none,
+                    fontSize: 30),
+              ),
+              Text("Krave",
+                  style: TextStyle(
+                      color: tealColor,
+                      decoration: TextDecoration.none,
+                      fontSize: 30)),
+            ],
+          ),
+          slides.elementAt(currentSlide),
           MainButton(
               text: "Get Started",
               onPressed: () {
-                //todo: add function to the button
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const MyHomePage(title: "Welcome to home page")));
               }),
           //sign up text
           Row(
@@ -69,7 +108,10 @@ class _OnBoardingState extends State<OnBoarding> {
               ),
               GestureDetector(
                 onTap: () {
-                  //todo: navigate to sign up
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RegisterScreen()));
                 },
                 child: Text(
                   "Sign Up",
@@ -86,53 +128,14 @@ class _OnBoardingState extends State<OnBoarding> {
       ),
     );
   }
-}
-
-class OnBoardingSlide extends StatelessWidget {
-  final Image image;
-  final String mainText;
-  final String secondaryText;
-
-  const OnBoardingSlide(
-      {Key? key,
-      required this.image,
-      required this.mainText,
-      required this.secondaryText})
-      : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        //todo: test dimensions
-        SizedBox(
-          height: 250,
-          child: image,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(8, 15, 8, 15),
-          child: Text(
-            mainText,
-            style: const TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 22.0,
-                color: Color.fromARGB(255, 46, 46, 46)),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        //main text
-        Text(
-          secondaryText,
-          style: const TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 13.0,
-              color: Color.fromARGB(255, 157, 157, 157)),
-          textAlign: TextAlign.center,
-        )
-      ],
-    );
+  void initState() {
+    Timer.periodic(const Duration(seconds: 6), (timer) {
+      setState(() {
+        currentSlide = (currentSlide + 1) % slides.length;
+      });
+    });
+    super.initState();
   }
 }
-//todo: add slider to onBoardingSlide with Gesture detector and state changer
